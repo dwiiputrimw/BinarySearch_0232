c9c33577d294062198565ea811c507c51acd3e1b
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-14 11:12:58 +0700

    Memasukkan elemen pada Array

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int element[10];
 int nPanjang;
 int x;
 
-int main()
+void Input()
 {
-    return 0;
+    while (true)
+    {
+        cout <<"Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else 
+        {
+            cout <<"\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
 }
