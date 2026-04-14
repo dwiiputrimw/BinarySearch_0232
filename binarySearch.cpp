40921e12ed275b6b2ab04f3ef1f7d13b8c82834d
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-14 11:20:01 +0700

    Menambahkan do pada ditemukannya indeks

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -82,7 +82,20 @@ void display()
 
         int low = 0;
         int high = nPanjang - 1;
+        
+        do 
+        {
+            int mid = (low + high) /2;
 
+            if (element[mid] == x)
+            {
+                cout << "n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
 
 
 
